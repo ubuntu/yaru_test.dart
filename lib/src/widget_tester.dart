7400cace9573af03c5933b9ea3f4518a61027148
J-P Nurmi <jpnurmi@ubuntu.com>
2023-05-16 18:54:31 +0200

    Restore tapLink()

--- lib/src/widget_tester.dart
+++ lib/src/widget_tester.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'common_finders.dart';
@@ -6,6 +8,20 @@ import 'custom_matchers.dart';
 extension YaruWidgetTester on WidgetTester {
   /// Taps a button specified by its [text].
   Future<void> tapButton(String text) => tap(find.button(text));
+
+  /// Taps a link with the given [label].
+  Future<void> tapLink(String label) async {
+    expect(find.byWidgetPredicate((widget) {
+      if (widget is RichText) {
+        final link = widget.findLink(label);
+        if (link != null) {
+          (link.recognizer as TapGestureRecognizer).onTap!();
+          return true;
+        }
+      }
+      return false;
+    }), findsOneWidget);
+  }
 
   /// Taps a radio specified by its [value].
   Future<void> tapRadio<T>(T value) => tap(find.radio(value));
@@ -16,5 +32,20 @@ extension YaruWidgetTester on WidgetTester {
     if (isChecked.matches(finder, {}) != value) {
       return tap(finder);
     }
+  }
+}
+
+extension on RichText {
+  TextSpan? findLink(String label) {
+    TextSpan? span;
+    text.visitChildren((child) {
+      if (child is TextSpan &&
+          child.text == label &&
+          child.recognizer is TapGestureRecognizer) {
+        span = child;
+      }
+      return span == null;
+    });
+    return span;
   }
 }
